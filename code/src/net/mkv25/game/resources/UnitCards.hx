dd834057ac5c11becbbe9cb51ec9b3a8092f0b63
John Beech <github@mkv25.net>
2014-08-30 02:37:55 +0100

    Added in new unit icons for different base sizes to match cards.

--- code/src/net/mkv25/game/resources/UnitCards.hx
+++ code/src/net/mkv25/game/resources/UnitCards.hx
@@ -15,45 +15,45 @@ class UnitCards
 			cost: 10,
 			base: true,
 			pictureTile: 7,
-			iconOffset: 1
+			iconOffset: 2
 		},
 		"Outpost": {
 			strength: 3,
 			cost: 20,
 			base: true,
 			pictureTile: 8,
-			iconOffset: 1
+			iconOffset: 3
 		},
 		"Metroplex": {
 			strength: 4,
 			cost: 40,
 			base: true,
 			pictureTile: 9,
-			iconOffset: 1
+			iconOffset: 4
 		},
 		"Assault Team": {
 			strength: 2,
 			cost: 3,
 			pictureTile: 12,
-			iconOffset: 2
+			iconOffset: 5
 		},
 		"Armoured Core": {
 			strength: 3,
 			cost: 6,
 			pictureTile: 13,
-			iconOffset: 3
+			iconOffset: 6
 		},
 		"Titan Force": {
 			strength: 4,
 			cost: 12,
 			pictureTile: 14,
-			iconOffset: 4
+			iconOffset: 7
 		},
 		"Capital Army": {
 			strength: 5,
 			cost: 24,
 			pictureTile: 15,
-			iconOffset: 5
+			iconOffset: 8
 		}
 	};
 }
