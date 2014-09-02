9940edd8f80fcd096136cc9934958f80eb17328d
John Beech <github@mkv25.net>
2014-09-02 23:20:59 +0100

    Optimized imports.

--- code/src/net/mkv25/game/ui/CardHolderForPurchaseUI.hx
+++ code/src/net/mkv25/game/ui/CardHolderForPurchaseUI.hx
@@ -1,13 +1,5 @@
 package net.mkv25.game.ui;
 
-import flash.display.Graphics;
-import flash.events.MouseEvent;
-import net.mkv25.base.core.Signal;
-import net.mkv25.base.ui.BitmapUI;
-import net.mkv25.game.event.EventBus;
-import net.mkv25.game.models.PlayableCard;
-import net.mkv25.game.ui.CardHolderUI;
-
 class CardHolderForPurchaseUI extends CardHolderUI
 {
 	public function new() 
