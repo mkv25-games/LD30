9c7c6ddbb2adb679431a7f32e493061c151c03bd
John Beech <github@mkv25.net>
2014-10-13 08:01:28 +0100

    Added in support to navigate/change page using left and right keys. No buttons yet.

--- code/src/net/mkv25/game/screens/BeginnersGuideScreen.hx
+++ code/src/net/mkv25/game/screens/BeginnersGuideScreen.hx
@@ -14,6 +14,8 @@ import openfl.Assets;
 
 class BeginnersGuideScreen extends Screen
 {
+	private var pageAssets:Array<String>;
+	private var currentPageIndex:Int;
 	
 	public function new() 
 	{
@@ -22,7 +24,26 @@ class BeginnersGuideScreen extends Screen
 	
 	override private function setup():Void 
 	{
-		setBackground("img/guide/guide-p1.png");
+		this.pageAssets = [
+			"img/guide/guide-p1.png",
+			"img/guide/guide-p2.png",
+			"img/guide/guide-p3.png",
+			"img/guide/guide-p4.png",
+			"img/guide/guide-p5.png",
+			"img/guide/guide-p6.png"
+		];
+		
+		showPage(0);
+	}
+	
+	function showPage(page:Int):Void
+	{
+		var numPages = pageAssets.length;
+		currentPageIndex = (numPages + page) % numPages;
+		
+		var assetPath = pageAssets[currentPageIndex];
+		
+		setBackground(assetPath);
 	}
 	
 	override public function handleKeyAction(event:KeyboardEvent):Void
@@ -34,15 +55,15 @@ class BeginnersGuideScreen extends Screen
 		
 		if (event.keyCode == Keyboard.LEFT)
 		{
-			Index.screenController.showScreen(Index.introScreen);
+			showPage(currentPageIndex - 1);
 		}
 		else if (event.keyCode == Keyboard.RIGHT)
 		{
-			
+			showPage(currentPageIndex + 1);
 		}
-		else if (event.keyCode == Keyboard.DOWN)
+		else if (event.keyCode == Keyboard.DOWN || event.keyCode == Keyboard.UP)
 		{
-			
+			Index.screenController.showScreen(Index.introScreen);
 		}
 	}
 }
